fc64678e70b62188fc7d9d3062095e1169fc440a
Svetlana Ignatchik <lightbug89@gmail.com>
2023-01-19 16:40:37 +0300

    Task025

---
+++ Task025/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+
+int GetExtentOfNumber (int numberA, int numberB)
+{
+  int result = 1;
+  for (int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+  return result;
+}
+  Console.Write("Введите число A: ");
+  int numberA = int.Parse(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = int.Parse(Console.ReadLine());
+
+  int exponentiation = GetExtentOfNumber(numberA, numberB);
+  Console.WriteLine($"Число А в степени В = " + exponentiation);
