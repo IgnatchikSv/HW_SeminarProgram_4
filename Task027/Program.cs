2a28a6efd1e4fb10afc5a7b30b33eb8aacc0e964
Svetlana Ignatchik <lightbug89@gmail.com>
2023-01-19 17:16:13 +0300

    Task027

---
+++ Task027/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11 82 -> 10 9012 -> 12
+
+Console.Clear();
+
+int getUserValue(string message)
+{
+  Console.WriteLine(message);
+  int userValue = int.Parse(Console.ReadLine());
+  return userValue;
+}
+
+int GetSumOfDigits (int userValue)
+{
+   int sum = 0;
+   while (userValue > 0)
+   {
+    sum = sum + userValue % 10;
+    userValue = userValue / 10;
+   }
+   return sum;
+}
+int number = getUserValue ($"Введите число:");
+int sum = GetSumOfDigits (number);
+Console.WriteLine($"Сумма цифр в числе = " + sum);
